b967fb1d6a88232ae03886cb1b855f63ccb0643c
mikeh-dev <mikeh112@hotmail.com>
2022-09-14 12:04:25 +0100

    Added about and contact controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'application#hello'
   
   get 'static_pages/home'
+  get 'static_pages/about'
   get 'static_pages/help'
+  get 'static_pages/contact'
+
 end
