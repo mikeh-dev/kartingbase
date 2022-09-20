64bba53a08f91cd6718e558c8059fb87c5d56b51
mikeh-dev <mikeh112@hotmail.com>
2022-09-20 23:49:37 +0100

    Added index to users email, removed fixtures, adjusted validations and tests to suit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   validates :name, presence: true, length: { maximum: 50}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true, length: { maximum: 255}, uniqueness: {case_sensitive: false}
+  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true, length: { maximum: 255}, uniqueness: true
 
   
 
