49c0f2de54679b8e9b6dbe935fbe7e06f21711f8
mikeh-dev <mikeh112@hotmail.com>
2022-09-20 23:39:15 +0100

    Added more test and validaitons for name and email in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50}
-  validates :email, presence: true, length: { maximum: 255}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true, length: { maximum: 255}, uniqueness: {case_sensitive: false}
 
   
 
