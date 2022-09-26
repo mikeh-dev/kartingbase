dc855d86869f7528de22ab9bd178e1038c482ac2
mikeh-dev <mikeh112@hotmail.com>
2022-09-26 23:38:24 +0100

    Added flash message on sign up and redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = "Welcome to KartBase!"
+      redirect_to @user
     else
      render 'new', status: :unprocessable_entity    
-   end
-
+    end
   end
 end
 
@@ -22,5 +22,5 @@ end
 private
 
   def user_params
-   params.require(:user).permit(:name, :email,  :password, :password_confirmation)
+   params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
