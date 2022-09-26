e45455980bd3e38f4f82721bf87fffbf408c95b4
mikeh-dev <mikeh112@hotmail.com>
2022-09-26 23:06:14 +0100

    Added errors partial, added bs classes to form, added basic styling for error messages, added strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       
     else
@@ -17,3 +17,10 @@ class UsersController < ApplicationController
 
   end
 end
+
+
+private
+
+  def user_params
+   params.require(:user).permit(:name, :email,  :password, :password_confirmation)
+  end
