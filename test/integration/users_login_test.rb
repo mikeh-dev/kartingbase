fb3eb00368af9d0affe12a1b087b1bddfc00aa56
mikeh-dev <mikeh112@hotmail.com>
2022-09-27 21:24:11 +0100

    Added turbo, turbo and stimulus to handle js for menu opening and closing. More basic styling, finished sessions controller, started login function

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  
+  test 'no flash message repeat' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: {
+      session: {
+        email: 'user@invalid',
+        password: 'incorrect'
+      }
+    }
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+end
