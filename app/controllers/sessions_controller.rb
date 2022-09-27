f59ea4c7b6ea6c4dcb5caf25c8047731aa383762
mikeh-dev <mikeh112@hotmail.com>
2022-09-27 22:55:42 +0100

    Added more thorough test for logout, added destro functionality in sessions controller, added some helper functions for testa and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       reset_session
       log_in user
       redirect_to user
@@ -17,7 +17,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    log_out
+    redirect_to root_url, status: :see_other
   end
 end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,7 +6,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test 'login with valid information' do
+  test 'login with valid email/invalid password' do
+    post login_path, params: {
+      session: {
+        email: @user.email,
+        password: 'invalid'
+      }
+    }
+    assert_not is_logged_in?
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test 'login with valid information followed by a logout' do
     post login_path, params: {
       session: {
         email: @user.email,
@@ -19,6 +34,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
   
   test 'no flash message repeat' do
@@ -36,4 +59,5 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
 end
