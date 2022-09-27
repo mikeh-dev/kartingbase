a7a0db68e52cb0d41446720115109b4927b1931f
mikeh-dev <mikeh112@hotmail.com>
2022-09-28 00:49:20 +0100

    Added sessions helper tests to verify user for remember session function, added remember button, basic styling, finished remember me function with tests to suit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
     
-    if user&.authenticate(params[:session][:password])
+    if @user&.authenticate(params[:session][:password])
       reset_session
-      remember user
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
@@ -22,4 +22,3 @@ class SessionsController < ApplicationController
     redirect_to root_url, status: :see_other
   end
 end
-
