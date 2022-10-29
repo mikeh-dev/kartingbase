21e0973f21d6f39541011e4dace0062ee91d7243
mikeh-dev <mikeh112@hotmail.com>
2022-10-29 23:29:52 +0100

    Amended user model, seeds and fixtures to include the essentials for user activations, reseeded DB, slight refactoring in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50}
 
@@ -40,5 +41,16 @@ class User < ApplicationRecord
 
   def session_token
     remember_digest || remember
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Mike Howard",
   email: "mikeh112@hotmail.com",
   password:              "password",
   password_confirmation: "password",
-  admin: true)
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -12,5 +14,7 @@ password = "password"
 User.create!(name:  name,
     email: email,
     password:              password,
-    password_confirmation: password)
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
 end
