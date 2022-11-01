aacbc272ae5456fac84a989f46b979e9b881f525
mikeh-dev <mikeh112@hotmail.com>
2022-11-01 21:41:44 +0000

    trying to fix Heroku deploy error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = https://kartbase22.herokuapp.com
+  #host = https://kartbase22.herokuapp.com
 
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
