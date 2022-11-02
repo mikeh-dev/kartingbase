cbde51d5f6543a9af7fcb6b6fb45b78be9e8b841
mikeh-dev <mikeh112@hotmail.com>
2022-11-02 22:45:02 +0000

    Completed all password reset functionality aside from the tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -17,6 +18,21 @@ class PasswordResetsController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
   end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      @user.forget
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
   
 
 
@@ -25,6 +41,11 @@ class PasswordResetsController < ApplicationController
   end
 
   private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
@@ -33,6 +54,13 @@ class PasswordResetsController < ApplicationController
       unless (@user && @user.activated? &&
               @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
+      end
+    end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -62,7 +62,11 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
-  private
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+    private
 
   def downcase_email
     email.downcase!
