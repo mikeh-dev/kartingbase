9cc2b5dc5dedc1e88f472f037383b8cbec4817b5
mikeh-dev <mikeh112@hotmail.com>
2022-11-07 23:19:59 +0000

    Added the microposts destory and tests to suit

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,12 +8,15 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost Created!"
       redirect_to root_url #not sure about this redirect, surely should go to microposts index?
     else
-     render 'static_pages/home', status: :unprocessable_entity    
+      @feed_items = current_user.feed.paginate(page: params[:page])
+      render 'static_pages/home', status: :unprocessable_entity    
     end
   end
 
   def destroy
-
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_back_or_to(root_url, status: :see_other)
   end
 
 
@@ -21,6 +25,12 @@ class MicropostsController < ApplicationController
   def micropost_params
     params.require(:micropost).permit(:content)
   end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other if @micropost.nil?
+  end
+
 
   
 end
