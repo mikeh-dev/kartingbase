7c87bedbdd64de25ca2d93a12ea4a67026068296
mikeh-dev <mikeh112@hotmail.com>
2022-11-07 22:21:51 +0000

    Split logged in home and logged out home into partials, added micropost entry form, changed error messages partial to work with both users and microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,26 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost Created!"
+      redirect_to root_url #not sure about this redirect, surely should go to microposts index?
+    else
+     render 'static_pages/home', status: :unprocessable_entity    
+    end
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
 
   
 end
