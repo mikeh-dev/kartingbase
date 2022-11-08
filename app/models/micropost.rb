cac267d7006ddad478e4ba17b777f6b80b1c3d84
mikeh-dev <mikeh112@hotmail.com>
2022-11-08 00:59:44 +0000

    Added image resize functionality and prevented users from uploding img over 5mb and restricted file type on browser with JS and server side

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,14 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  has_one_attached :image
-  
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
   validates :user_id, presence: true
   validates :content, length: { minimum: 0, maximum: 140 }, presence: true
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+    message: "must be a valid image format" },
+size:         { less_than: 5.megabytes,
+    message:   "should be less than 5MB" }
+
 end
