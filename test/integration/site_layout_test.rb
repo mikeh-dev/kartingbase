c155daa22c1f2008aefdc49f37cf8f18e13cf264
mikeh-dev <mikeh112@hotmail.com>
2022-11-30 23:32:41 +0000

    Added tests for entries and tracks links in nav

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,6 +11,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", tracks_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", users_path, count: 0
@@ -22,6 +23,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", entries_path
     assert_select "a[href=?]", edit_user_path(@user)
     assert_select "a[href=?]", user_path(@user)
   end
