4c988b836a6304b4da68946d624e12997bfa26ae
mikeh-dev <mikeh112@hotmail.com>
2022-11-30 23:19:07 +0000

    Removed help from Main Nav Bar, fixed help link in footer nav, began basic controller test for entries and tracks

--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class TracksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get tracks_path
+    assert_response :success
+  end
 end
