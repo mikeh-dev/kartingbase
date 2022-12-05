908e0da19bcb9020d79f22b795834bfa3a8b2d8f
mikeh-dev <mikeh112@hotmail.com>
2022-12-05 23:21:10 +0000

    Lost my way with features and didn't use TDD, abandoning branch and will start again

---
+++ db/migrate/20221205221654_add_foreign_key_to_entry.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToEntry < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :entries, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_012250) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_221654) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_012250) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
   create_table "microposts", force: :cascade do |t|
@@ -88,5 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_012250) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "entries", "users"
   add_foreign_key "microposts", "users"
 end
